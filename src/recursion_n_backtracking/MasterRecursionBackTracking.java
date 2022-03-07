787073786731cfe8fd6e6eee526c3d692d485aa2
Rishabh Jaiswal <rshbh.jaiswal@gmail.com>
2022-03-08 03:33:07 +0530

    Find power using recursion

--- src/recursion_n_backtracking/MasterRecursionBackTracking.java
+++ src/recursion_n_backtracking/MasterRecursionBackTracking.java
@@ -4,5 +4,12 @@ public class MasterRecursionBackTracking {
 
 	public static void main(String args[]) {
 		System.out.println("Start execution of the Recursion & BackTracking programs");
+		System.out.println(PowerN.isPower2(5));
+		System.out.println(PowerN.isPower2(4));
+		System.out.println(PowerN.isPower2(8));
+		System.out.println(PowerN.isPower3(8));
+		System.out.println(PowerN.isPower4(8));
+		System.out.println(PowerN.isPower3(9));
+		System.out.println(PowerN.isPower4(4));
 	}
 }
